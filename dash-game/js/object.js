225bbba947fd9a0d7fb8543b52be2c3e6539a582
heidttayven-lang <heidt.tayven@prairiesouth.ca>
2026-05-28 11:08:52 -0600

    Create object.js

---
+++ dash-game/js/object.js
@@ -0,0 +1,17 @@
+const objects = [
+  {
+    x: 500,
+    y: 500,
+    width: 40,
+    height: 40,
+    type: "spike"
+  },
+
+  {
+    x: 800,
+    y: 450,
+    width: 50,
+    height: 50,
+    type: "block"
+  }
+];
