e49806ba240e4b8e6193d4ef7fa5cb95faaa2d20
heidttayven-lang <heidt.tayven@prairiesouth.ca>
2026-05-28 11:07:13 -0600

    Update main.js

--- dash-game/main.js
+++ dash-game/main.js
@@ -1,1 +1,26 @@
+const canvas = document.getElementById("game");
+const ctx = canvas.getContext("2d");
 
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let keys = {};
+
+document.addEventListener("keydown", e => {
+  keys[e.code] = true;
+});
+
+document.addEventListener("keyup", e => {
+  keys[e.code] = false;
+});
+
+function gameLoop() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  updatePlayer();
+  updateLevel();
+
+  requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
