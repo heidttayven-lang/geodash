c26bbd8b9503c1f04ed818bf0e58d8b77c3b9835
heidttayven-lang <heidt.tayven@prairiesouth.ca>
2026-05-28 11:09:15 -0600

    Create portal.js

---
+++ dash-game/js/portal.js
@@ -0,0 +1,26 @@
+const portals = [
+
+  {
+    x: 700,
+    y: 450,
+    width: 50,
+    height: 100,
+    mode: "ship"
+  },
+
+  {
+    x: 1200,
+    y: 450,
+    width: 50,
+    height: 100,
+    mode: "wave"
+  },
+
+  {
+    x: 1700,
+    y: 450,
+    width: 50,
+    height: 100,
+    mode: "ball"
+  }
+];
